7a773e58c7d1beb06fada8b6e2b622e0393f229d
lanyulei <lanyulei@yidian-inc.com>
2024-10-24 00:38:49 +0800

    feat: add menu

--- src/components/layout/menu/index.vue
+++ src/components/layout/menu/index.vue
@@ -1,34 +1,40 @@
 <template>
-  <div class="helper-layout-menu">
-    <div class="helper-logo">
-      <img :src="LogoIcon" alt="" width="38" />
-    </div>
-    <el-menu default-active="1" :collapse="true" class="helper-el-menu">
-      <el-menu-item index="1" @click="handleRouter('/home')">
-        <el-icon><Reading /></el-icon>
-        <template #title>首页</template>
-      </el-menu-item>
-      <el-menu-item index="2" @click="handleRouter('/tools')">
-        <el-icon><CopyDocument /></el-icon>
-        <template #title>工具</template>
-      </el-menu-item>
-      <el-menu-item index="3" @click="handleRouter('/chat')">
-        <el-icon><ChatLineRound /></el-icon>
-        <template #title>聊天</template>
-      </el-menu-item>
-      <el-menu-item index="4" @click="handleRouter('/settings')">
-        <el-icon><Setting /></el-icon>
-        <template #title>设置</template>
-      </el-menu-item>
-    </el-menu>
-    <el-divider />
-    <div class="helper-avatar">
-      <img :src="UserAvatarIcon" alt="" width="40" />
+  <div :style="{ height: globalHeight + 'px' }">
+    <div class="helper-layout-menu">
+      <div class="helper-logo">
+        <img :src="LogoIcon" alt="" width="38" />
+      </div>
+      <el-menu default-active="1" :collapse="true" class="helper-el-menu">
+        <el-menu-item index="1" @click="handleRouter('/home')">
+          <el-icon><Reading /></el-icon>
+          <template #title>首页</template>
+        </el-menu-item>
+        <el-menu-item index="2" @click="handleRouter('/tools')">
+          <el-icon><CopyDocument /></el-icon>
+          <template #title>工具</template>
+        </el-menu-item>
+        <el-menu-item index="3" @click="handleRouter('/chat')">
+          <el-icon><ChatLineRound /></el-icon>
+          <template #title>聊天</template>
+        </el-menu-item>
+        <el-menu-item index="4" @click="handleRouter('/settings')">
+          <el-icon><Setting /></el-icon>
+          <template #title>设置</template>
+        </el-menu-item>
+      </el-menu>
+      <div class="helper-menu-foot">
+        <el-divider />
+        <div class="helper-avatar">
+          <img :src="UserAvatarIcon" alt="" width="40" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
 import {
   ChatLineRound,
   CopyDocument,
@@ -46,6 +52,12 @@ const router = useRouter();
 const handleRouter = (path: string) => {
   router.push(path);
 };
+
+const globalHeight = ref(window.innerHeight - 30);
+
+window.onresize = () => {
+  globalHeight.value = window.innerHeight - 30;
+};
 </script>
 
 <style lang="scss" scoped>
@@ -53,6 +65,13 @@ const handleRouter = (path: string) => {
   width: 64px;
   border-radius: 10px;
   border: 1px solid #ddd !important;
+  height: 100%;
+  .helper-menu-foot {
+    position: absolute;
+    left: 28px;
+    bottom: 15px;
+  }
+
   .helper-el-menu {
     border-right: none;
   }
